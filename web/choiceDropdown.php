d451bac2bd888acc715036dde9d7eb5ad6eafcd1
Everest Oreizy <everest@oreizy.com>
2018-11-18 17:53:27 -0800

    through B1

---
+++ web/choiceDropdown.php
@@ -0,0 +1,10 @@
+<select id="choice">
+	<option value="0">Beginning</option>
+	<option value="6">A</option>
+	<option value="36">B</option>
+	<option value="85">C: Opening Chorus</option>
+	<option value="131">D</option>
+	<option value="163">E</option>
+	<option value="196">A1</option>
+	<option value="227">B1: Ithaca</option>
+</select>
