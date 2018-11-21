1f4465ae8a0679c39dc9c08c4dac3fda38659847
Everest Oreizy <everest@oreizy.com>
2018-11-20 23:33:24 -0800

    Styles

--- web/sectionsDropdown.php
+++ web/sectionsDropdown.php
@@ -4,20 +4,19 @@ require_once("sections.php");
 ?>
 <select id="sections" onchange="setTime('sections')">
 <option id="sectionsDefault" value='0'>Rehearsal Marks</option>
-
+<optgroup label="Prologue">
 <?php
 for($i=0; $s = $allSectionNames[$i]; $i++){
 	echo "<option value='${$s}'>$s</option>";
-	echo "<br>";
 	if($i==count($allSectionNames)){
 		break;
 	}
-	/*
-	$t = int(substr($allSectionNames[$i+1], 1));
-	if($t > int(substr($s, 1))){
+	
+	$t = substr($allSectionNames[$i+1], 1);
+	if($t > substr($s, 1)){
 		echo "</optgroup><optgroup label='Scene $t'>";
 	}
-	*/
+	
 }
 ?>
 </select>
